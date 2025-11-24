0c29965f994c0ec88d284025bd3d9f3b201e7e25
Jorge Velasco <jorge@jorge-velasco-games.com>
2025-11-24 21:50:10 +0100

    Now Thomas changes position when you take the photos.

---
+++ my_fair_damnation_godot/npcs/neighboor_main_instance.gd
@@ -0,0 +1,28 @@
+extends NpcInstance
+
+const NEIGHBOOR_SITTING_COL = preload("uid://dkvqox5nf2mg3")
+const NEIGHBOOR_DEPRAVED_COL = preload("uid://c5ra3cph4d0it")
+
+@export var skyler_photos : Item
+
+@export var depraved_position : Vector3
+@export var depraved_rotation: Vector3
+@export var sitting_position : Vector3
+@export var sittin_rotation : Vector3
+@onready var collision_shape_3d: CollisionShape3D = $Interactable/CollisionShape3D
+
+
+func _activate_instance() -> void:
+	super()
+	if player.player_inventory.items.has(skyler_photos):
+		global_position = sitting_position
+		global_rotation.y = deg_to_rad(-90)
+		animation_player.play("idle_sit")
+		npc_static_body_3d.hide()
+		collision_shape_3d.shape = NEIGHBOOR_SITTING_COL
+	else:
+		global_position = depraved_position
+		global_rotation.y = deg_to_rad(180)
+		animation_player.play("idle_depraved")
+		npc_static_body_3d.show()
+		collision_shape_3d.shape = NEIGHBOOR_DEPRAVED_COL
